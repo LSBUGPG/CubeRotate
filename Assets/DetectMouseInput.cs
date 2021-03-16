507097a4327f8446278f1005e6066b54eeab5410
Paul Sinnett <paul.sinnett@gmail.com>
2021-03-16 14:08:48 +0000

    Initial commit

---
+++ Assets/DetectMouseInput.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RotateCube))]
+public class DetectMouseInput : MonoBehaviour
+{
+    Vector3 downPosition;
+    RotateCube cube;
+
+    void Start()
+    {
+        cube = GetComponent<RotateCube>();
+    }
+
+    void OnMouseDown()
+    {
+        downPosition = Input.mousePosition;
+    }
+    void OnMouseUp()
+    {
+        Vector3 move = Input.mousePosition - downPosition;
+        if (Mathf.Abs(move.x) > Mathf.Abs(move.y))
+        {
+            if (move.x > 0)
+            {
+                cube.MoveRight();
+            }
+            else
+            {
+                cube.MoveLeft();
+            }
+        }
+        else
+        {
+            if (move.y > 0)
+            {
+                cube.MoveUp();
+            }
+            else
+            {
+                cube.MoveDown();
+            }
+        }
+    }
+}
---
+++ Assets/RotateCube.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public float speed = 1.0f;
+    float time = 0.0f;
+    Quaternion start;
+    Quaternion end;
+    Queue<Quaternion> moves = new Queue<Quaternion>();
+
+    void Move(Quaternion direction)
+    {
+        moves.Enqueue(direction);
+    }
+
+    public void MoveRight()
+    {
+        Move(Quaternion.Euler(0.0f, -90.0f, 0.0f));
+    }
+
+    public void MoveLeft()
+    {
+        Move(Quaternion.Euler(0.0f, 90.0f, 0.0f));
+    }
+
+    public void MoveUp()
+    {
+        Move(Quaternion.Euler(90.0f, 0.0f, 0.0f));
+    }
+
+    public void MoveDown()
+    {
+        Move(Quaternion.Euler(-90.0f, 0.0f, 0.0f));
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime * speed;
+        if (time > 1.0f)
+        {
+            time = 1.0f;
+            transform.rotation = end;
+
+            if (moves.Count > 0)
+            {
+                Quaternion direction = moves.Dequeue();
+                start = transform.rotation;
+                end = direction * start;
+                time = 0.0f;
+            }
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(start, end, time);
+        }
+    }
+}
